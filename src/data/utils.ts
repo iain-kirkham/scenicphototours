dcfcd43f0298a9a9995b6c89251e8384fa29db66
Iain <iain.kirkham@outlook.com>
2026-08-12 01:46:24 +0100

    fix: add social media links for Instagram and LinkedIn

--- src/data/utils.ts
+++ src/data/utils.ts
@@ -5,3 +5,8 @@ export const links: { href: string; text: string }[] = [
 	{ href: "/about/", text: "About" },
 	{ href: "/contact/", text: "Contact" },
 ];
+
+export const socials: { name: string; href: string; color: string }[] = [
+	{ name: "Instagram", href: "https://www.instagram.com/scenicphototours", color: "#E1306C" },
+	{ name: "LinkedIn", href: "https://www.linkedin.com/in/sarahphilp/", color: "#0A66C2" },
+];
